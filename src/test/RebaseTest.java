1b87a8cb91baa2827f420991484c7f8f7bf0e7c1
meiyukai <meiyukai.myk@alibaba-inc.com>
2022-12-08 14:25:04 +0800

    add A

---
+++ src/test/RebaseTest.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author : meiyukai
+ * @date :  2022/12/8 14:23
+ * @description :
+ */
+public class RebaseTest {
+    public static void main(String[] args) {
+        System.out.println(" AAAAA ");
+    }
+}
