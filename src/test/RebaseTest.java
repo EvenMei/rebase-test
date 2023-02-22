68b79000592114f3af49df9e8707535568144d20
meiyukai <meiyukai@meiyukaideMacBook-Pro.local>
2023-02-22 20:23:59 +0800

    add 2

--- src/test/RebaseTest.java
+++ src/test/RebaseTest.java
@@ -8,5 +8,6 @@ package test;
 public class RebaseTest {
     public static void main(String[] args) {
         System.out.println(" --- 1 --- ");
+        System.out.println(" --- 2 --- ");
     }
 }
